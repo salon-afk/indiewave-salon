32dc9199a2782d2aa3b0ce06555eb06721556f4d
Salon <salon@student.com>
2026-05-07 02:25:49 +0100

    fix duplicate port

--- music-platform/backend/server.js
+++ music-platform/backend/server.js
@@ -328,7 +328,6 @@ app.use((req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`IndieWave running on port ${PORT}`);
