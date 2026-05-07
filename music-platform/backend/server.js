d934e2ebc91ccb5ccc25cb892478f764b63eb1c5
Salon <salon@student.com>
2026-05-07 03:07:42 +0100

    fix azure port

--- music-platform/backend/server.js
+++ music-platform/backend/server.js
@@ -10,7 +10,7 @@ const fs = require("fs");
 const mm = require("music-metadata");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 const SECRET = "indiewave_secret_key";
 
 const blobServiceClient = BlobServiceClient.fromConnectionString(
